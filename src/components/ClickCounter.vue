444c6c38de2085701d61efe2f09d56822c5f69cd
Emmanuel Ametepee <emmanuelamet11@gmail.com>
2021-12-15 23:24:51 -0800

    Mixins

---
+++ src/components/ClickCounter.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <button @click="IncrementCount">Click {{count}} times</button>
+    </div>
+</template>
+
+<script>
+import CounterMixin from '../mixins/Counter'
+
+    export default {
+        name: 'ClickCounter',
+        mixins: [CounterMixin]
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/HoverCounter.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h2 @mouseover="IncrementCount">Mouse Hover {{count}} times</h2>
+    </div>
+</template>
+
+<script>
+import CounterMixin from '../mixins/Counter'
+
+    export default {
+        name: 'HoverCounter',
+        mixins: [CounterMixin]
+    }
+</script>
+
+<style scoped>
+
+</style>
